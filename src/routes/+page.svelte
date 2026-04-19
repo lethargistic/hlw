d91b1716374716b64acf839d01b17f3089819b3f
Maksiks <maksiksking@gmail.com>
2026-04-19 18:52:51 +0300

    feat: some progress on the UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,67 +4,89 @@
             return;
         }
 
-
+        // TODO now: publish
     }
 </script>
 
-<header>
-    <div class="icon">
-
+<div class="full-wrap">
+    <div class="inner-wrap">
+        <header>
+            <div class="icon">
+                vlvt
+            </div>
+            <div class="account">
+                account
+            </div>
+        </header>
+        <main>
+            <section class="editor-seg">
+                <!-- TODO: 間もなく make these random -->
+                <input class="font-lexend" type="text" maxlength="32" id="title-input" placeholder="Title goes here...">
+                <textarea id="editor-area" placeholder="Write something..."></textarea>
+            </section>
+            <section class="publish-seg">
+                <button class="publish" aria-label="publish" onclick={handlePublish}>
+                    Publish
+                </button>
+                <p>Your link: <span>ewewewe</span></p>
+            </section>
+        </main>
+        <footer>
+            © 2026 i consume yuri media to survive
+        </footer>
     </div>
-    <div class="account">
-
-    </div>
-</header>
-<main>
-    <div class="main-wrap">
-        <section class="editor-seg">
-        <textarea maxlength="32" id="title-area">
-        e
-        </textarea>
-            <textarea id="editor-area">
-        aaa
-        </textarea>
-        </section>
-        <section class="output-seg">
-            <button class="publish" aria-label="publish" onclick={}>
-
-            </button>
-            <p>Your link: <span>ewewewe</span></p>
-        </section>
-    </div>
-</main>
+</div>
 
 <style>
-    main {
+    .full-wrap {
         display: grid;
         place-items: center;
 
-        & .main-wrap {
+        & header {
+            display: flex;
+
+            & .account {
+                margin-left: auto;
+            }
+        }
+
+        & .inner-wrap {
             max-width: 1024px;
             width: 100%;
+            padding: 3rem 0 4rem 0;
 
-            margin: 4rem 0 0 0;
+            & main {
+                & .editor-seg {
+                    & #title-input {
+                        font-size: 3rem;
+                        padding: 2rem 0 0 0;
 
-            & #title-area {
-                font-size: 2rem;
+                        font-weight: 350;
 
-                overflow-y: auto;
-            }
+                        text-overflow: ellipsis;
+                        overflow-y: auto;
+                    }
 
-            & #editor-area {
-                min-height: 100vh;
-            }
+                    & #editor-area {
+                        min-height: 100vh;
+                        padding: 2.5rem 0.15rem 0;
 
-            & textarea {
-                all: unset;
+                        font-size: 1.5rem;
 
-                width: 100%;
-                height: 100%;
-            }
+                        resize: none;
+                    }
 
-            & textarea:focus-visible {
-                outline: none !important;
+                    & textarea, input {
+                        all: unset;
+                        width: 100%;
+                        white-space: pre-wrap;
+                        overflow-wrap: break-word;
+                    }
+
+                    & textarea,input:focus-visible {
+                        outline: none !important;
+                    }
+                }
             }
         }
     }
---
+++ todo.md
@@ -0,0 +1,15 @@
+
+- [ ] todo now: ui
+- [ ] todo now: db
+- [ ] todo now: accounts
+- [ ] todo now: make title text alternate, come up with a lot of these
+- [ ] todo now: nojs
+- [ ] todo now: think of name ; vlvt.dev
+- [ ] todo now: proper a11y
+- [ ] todo now: revisit fonts ; i dont really like Lexend as header here
+- [ ] todo now: experiment with header to keep it minimalistic and out of attention unlike Rlim, how does monkeytype do it? ; it does so a UI element hmm
+
+list:
+Wring it on hai :) esc
+Write... esc
+Clip your thing to the board esc
